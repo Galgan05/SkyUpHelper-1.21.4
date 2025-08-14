6510d86918dd934dfc34ccc01f1a80fec0fbcfd8
Galgan05 <Galgan4170@protonmail.com>
2025-08-14 16:05:01 +0200

    Initial commit

---
+++ src/main/java/net/galgan/skyuphelper/SkyUpHelper.java
@@ -0,0 +1,16 @@
+package net.galgan.skyuphelper;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SkyUpHelper implements ModInitializer {
+	public static final String MOD_ID = "skyuphelper";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/galgan/skyuphelper/SkyUpHelperClient.java
@@ -0,0 +1,10 @@
+package net.galgan.skyuphelper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyUpHelperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
